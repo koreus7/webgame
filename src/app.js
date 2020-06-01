67c10f8c8f75187eb353d382e6549241f8f45a98
koreus7 <leo_mahon@hotmail.com>
2020-06-01 16:06:31 +0100

    test sprite and PIXI type definitions

--- src/app.js
+++ src/app.js
@@ -10,15 +10,38 @@ const app = new PIXI.Application(options);
 
 document.body.appendChild(app.view);
 
-const basicText = new PIXI.Text(`Hello World! ${window.__DEV_MODE ? 'dev' : 'release'}`);
-basicText.x = 50;
-basicText.y = 100;
+app.loader
+    .add('hero-sheet', '/assets/images/test-sprite.json')
+    .load(onLoaded);
 
-app.stage.addChild(basicText);
+function onLoaded() {
+    // Text
+    const basicText = new PIXI.Text(`Hello World! ${window.__DEV_MODE ? 'dev' : 'release'}`);
+    basicText.x = 50;
+    basicText.y = 100;
 
-app.view.addEventListener('click', function() {
-    const sound = new Pizzicato.Sound('./assets/sound/kick.wav', function() {
-        // Sound loaded!
-        sound.play();
-    });
-}, false);
+    app.stage.addChild(basicText);
+
+    // Sound
+    let sound = null;
+    app.view.addEventListener('click', function() {
+        if(sound) {
+            sound.play();
+        } else {
+            const sound = new Pizzicato.Sound('./assets/sound/kick.wav', function() {
+                // Sound loaded!
+                sound.play();
+            });
+        }
+
+    }, false);
+
+    // Sprites
+    const sheet = app.loader.resources["hero-sheet"].spritesheet;
+    const sprite = new PIXI.AnimatedSprite(sheet.animations["adventurer-run"]);
+    sprite.animationSpeed = 0.25;
+    sprite.play();
+    app.stage.addChild(sprite);
+
+    
+}
