eb27e66bc0016fc272fd1cce3332c771fdbeb4a1
Alex Jeffrey <alex@alexjeffrey.mee>
2020-07-10 23:30:16 +0100

    totally initial commit

--- src/assets.js
+++ src/assets.js
@@ -1,33 +1,3 @@
 
 // player
-export const PLAYER_GHOST_TEXTURE = './assets/images/player-ghost.png';
-export const PLAYER_IDLE_SHEET = './assets/images/player/idle.json';
-export const PLAYER_JUMP_TEXTURE = './assets/images/player/jump.png';
-export const PLAYER_RUN_SHEET = './assets/images/player/run.json';
-export const PLAYER_DRAG_SHEET  = './assets/images/player/drag.json';
-
-// items
-export const KNIFE_TEXTURE = './assets/images/knife.png';
-export const KNIFE_WOBBLE_SHEET = './assets/images/knife-wobble.json';
-
-// environment
-export const GROUND_TEXTURE = './assets/images/ground.png';
-export const CABIN_TEXTURE = './assets/images/cabin.png';
-export const CANDLE_SHEET = './assets/images/candle.json';
-export const CANDLE_LIGHT_TEXTURE = './assets/images/candle-light.png';
-export const BARREL_TEXTURE = './assets/images/barrel.png'
-export const OVEN_TEXTURE = './assets/images/oven.png';
-export const OVEN_SHEET = './assets/images/oven.json';
-
-// entities
-export const ENEMY_IDLE_TEXTURE = './assets/images/enemy-idle.png';
-export const ENEMY_CORPSE_TEXTURE = './assets/images/corpse.png';
-export const ENEMY_DEATH_SHEET = './assets/images/enemy-death.json';
-export const MEAT_0 = './assets/images/meat0.png';
-export const MEAT_1 = './assets/images/meat1.png';
-export const MEAT_2 = './assets/images/meat2.png';
-
-// GUI
-export const CHARGER_SHEET = './assets/images/charger.json';
-export const INTERACT_TEXTURE = './assets/images/interact.png';
-export const OVEN_TIMER_SHEET = './assets/images/oven-timer.json';
+export const AGENT_TEXTURE = './assets/images/agent.png';
